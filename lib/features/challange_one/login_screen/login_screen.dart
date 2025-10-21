cbdc9f797f76129a095a8ca1a382ac0afec38633
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-10-21 20:06:11 +0100

    login page

--- lib/features/challange_one/login_screen/login_screen.dart
+++ lib/features/challange_one/login_screen/login_screen.dart
@@ -6,10 +6,53 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Lets Get You Signed ')
-        ],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Lets Get You Signed in'),
+            Text('Please Enter your login info to continue'),
+        
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Email Address'),
+                TextField(
+                  decoration: InputDecoration(
+                    hint: Text('Enter email'),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.grey.shade200),
+                      borderRadius: BorderRadius.circular(20)
+                    )
+                  ),
+                ),
+        
+                SizedBox(height: 20,),
+                Text('Email Address'),
+                TextField(
+                  decoration: InputDecoration(
+                    hint: Text('Enter email'),
+                    suffixIcon: Icon(Icons.remove_red_eye),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.grey.shade200),
+                      borderRadius: BorderRadius.circular(20)
+                    )
+                  ),
+                ),
+
+                SizedBox(height: 20,),
+                
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Text('Forget Password', style: TextStyle(
+                    color: Colors.grey.shade800
+                  ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
