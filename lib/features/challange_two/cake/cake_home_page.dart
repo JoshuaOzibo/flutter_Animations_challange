a81cc8ef726ee3115fb6937e7ea12a8db54c9926
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-10-26 23:15:09 +0100

    home screen done

--- lib/features/challange_two/cake/cake_home_page.dart
+++ lib/features/challange_two/cake/cake_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/features/challange_two/cake/widgets/cake_button.dart';
 
 class CakeHomePage extends StatefulWidget {
   const CakeHomePage({super.key});
@@ -11,10 +12,57 @@ class _CakeHomePageState extends State<CakeHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(
-        mainAxisSize: MainAxisSize.max,
-      )),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CakeButton(value: Icon(Icons.access_alarms)),
+          
+                  Row(
+                    spacing: 20,
+                    children: [
+                      CakeButton(value: Icon(Icons.access_alarms)),
+                      CakeButton(value: Icon(Icons.access_alarms)),
+                    ],
+                  ),
+                ],
+              ),
+          
+              SizedBox(height: 20,),
+          
+              CakeButton(value: Text('Choose Your favourite')),
+          
+              Center(
+                heightFactor: 2,
+                child: Image.asset(
+                width: 300,
+                height: 300,
+                'images/cakes_images/cup_cake_one.webp',
+                )),
+          
+                Spacer(),
+          
+              Row(
+               spacing: 10,
+                children: [
+                  CakeButton(value: Icon(Icons.arrow_back_ios)),
+          
+                  Expanded(
+                    child: CakeButton(value: Text('Choose Your favourite')),
+                  ),
+          
+                  CakeButton(value: Icon(Icons.arrow_forward_ios)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
-  
 }
